71aa05ecfa78180201bcf54ae7b81e2d6ede86e8
ruegp <ru.egprojects@gmail.com>
2019-10-02 15:49:37 +0300

    Main branche

--- app/src/main/java/ru/egprojects/androidlab/MainActivity.kt
+++ app/src/main/java/ru/egprojects/androidlab/MainActivity.kt
@@ -9,20 +9,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        GainingOver().interact(Character.MORGAUSE, Character.MORGAN)
-        Character.MORGAN.becomeAVillain()
-        logE("Morgause has lured Morgan to her side and Morgan became a villain")
-        Killing().interact(Character.MORGAN, Character.UTER)
-        logE("Morgan killed Uter")
-        GainingOver().interact(Character.MERLIN, Character.MORGAN)
-        logE("Merlin brought Morgan back to the true path")
-        Character.ARTHUR.becomeAKing()
-        logE("Arthur became a king")
-    }
-
-    inline fun logE(msg: String) {
-        Log.e("MainActivity", msg)
     }
 
 }
